e9e671ce2d3c2e983e311cd08035f382bff84ec5
Sidharth Suvarna <siddarthsuvarna99@gmail.com>
2021-01-03 01:10:20 +0530

    Update signup.js

--- public/signup.js
+++ public/signup.js
@@ -6,10 +6,10 @@ require("firebase/firestore");
   // Set the configuration for your app
   // TODO: Replace with your project's config object
   var config = {
-    apiKey: "AIzaSyCzX8sKSTnJXEMCw_Q3MzJviP7Xr4U6kEI",
-    authDomain: "furniture-store-c3d57.firebaseapp.com",
-    databaseURL: "https://furniture-store-c3d57-default-rtdb.firebaseio.com/",
-    storageBucket: "furniture-store-c3d57.appspot.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: ""
   };
   firebase.initializeApp(config);
   // Get a reference to the database service
