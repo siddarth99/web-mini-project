67db8b6d9e9b2d9005ed9a6c7755b303d88e32ba
Sidharth Suvarna <siddarthsuvarna99@gmail.com>
2021-01-17 15:30:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ require("firebase/firestore");
 // Set the configuration for your app
 // TODO: Replace with your project's config object
 var config = {
-    apiKey: "AIzaSyCzX8sKSTnJXEMCw_Q3MzJviP7Xr4U6kEI",
-    authDomain: "furniture-store-c3d57.firebaseapp.com",
-    databaseURL: "https://furniture-store-c3d57-default-rtdb.firebaseio.com/",
-    storageBucket: "furniture-store-c3d57.appspot.com"
+    apiKey: "************",
+    authDomain: "**************",
+    databaseURL: "***************",
+    storageBucket: "**************"
 };
 
 firebase.initializeApp(config);
@@ -125,8 +125,8 @@ app.post('/checkout', function(req, res) {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'comfyhouse.contact@gmail.com',
-            pass: 'comfy@1234'
+            user: 'comfyhouse*********',
+            pass: '*******'
         }
     });
 
