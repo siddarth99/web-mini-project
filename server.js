85e2abea5065495f7fc1828b523029b9cf5491c4
Sidharth Suvarna <siddarthsuvarna99@gmail.com>
2021-01-18 16:27:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ require("firebase/firestore");
 // Set the configuration for your app
 // TODO: Replace with your project's config object
 var config = {
-    apiKey: "AIzaSyCzX8sKSTnJXEMCw_Q3MzJviP7Xr4U6kEI",
-    authDomain: "furniture-store-c3d57.firebaseapp.com",
-    databaseURL: "https://furniture-store-c3d57-default-rtdb.firebaseio.com/",
-    storageBucket: "furniture-store-c3d57.appspot.com"
+    
 };
 
 firebase.initializeApp(config);
@@ -126,7 +123,7 @@ app.post('/checkout', function(req, res) {
         service: 'gmail',
         auth: {
             user: 'comfyhouse.contact@gmail.com',
-            pass: 'comfy@1234'
+            pass: '*****'
         }
     });
 
